22df20d38447c64a62941ee738353cbdc8cdc536
Daniel P <danielpimen92@gmail.com>
2018-03-03 19:46:52 -0500

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Max',
+		photo: 'https://s3.amazonaws.com/imagesroot.rescuegroups.org/webpages/s135n9plaj4oari.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'2',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Chloe',
+		photo: 'https://usercontent2.hubstatic.com/12312363_f520.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'4',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dinky',
+		photo: 'https://vetstreet.brightspotcdn.com/dims4/default/871fdd9/2147483647/crop/0x0%2B0%2B0/resize/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F30%2Fee8ec09e8e11e0a2380050568d634f%2Ffile%2Fboston-terrier-1-062411.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'3',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Lily',
+		photo: 'http://cdn.akc.org/akccontentimages/BreedOfficialPortraits/hero/Bulldog.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'2',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Charlie',
+		photo: 'http://cdn-www.dailypuppy.com/dog-images/henri-the-mixed-breed_34304_2009-10-29_w450.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'4',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Chester',
+		photo: 'http://lh6.google.com/mjbmeister/R_Db6n83KCI/AAAAAAAADSA/jHMvjt0wD9A/s400/orange-persian-3.jpg',
+		scores: [
+			'1',
+			'4',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Einstein',
+		photo: 'http://cdn3-www.dogtime.com/assets/uploads/gallery/scottish-terrier-dog-breed-pictures/thumbs/thumbs_5-sittingsmile.jpg',
+		scores: [
+			'5',
+			'4',
+			'1',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Oreo',
+		photo: 'http://dogable.net/wp-content/uploads/2013/12/Chihuahua-Dachshund-mix2-300x224.png',
+		scores: [
+			'5',
+			'2',
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bubba',
+		photo: 'https://www.askideas.com/media/17/Orange-Maine-Coon-Cat-Face-Image.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'4',
+			'5',
+			'5',
+			'5',
+			'3',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'3',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
